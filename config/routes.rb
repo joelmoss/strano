32fd7b10fd5ecacb4b472497d48f98c1261f4cbe
Joel Moss <joel@developwithstyle.com>
2012-01-19 12:42:17 +0000

    Cannot edit a project

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Strano::Application.routes.draw do
   get 'sign_in', :to => 'sessions#new', :as => :sign_in
   delete 'sign_out', :to => 'sessions#destroy', :as => :sign_out
 
-  resources :projects do
+  resources :projects, :except => [:edit, :update] do
     get :pull, :on => :member
     resources :jobs, :except => [:new,:index] do
       get 'new/:task', :action => :new, :on => :collection, :as => :new
