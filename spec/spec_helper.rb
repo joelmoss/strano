fcea67b45277138a01c4100e4b9a6f65c5a52f1e
Joel Moss <joel@developwithstyle.com>
2012-04-04 14:51:59 +0100

    Removed ResqueSpec from spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,6 +43,4 @@ RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
-
-  config.before { ResqueSpec.reset! }
 end
